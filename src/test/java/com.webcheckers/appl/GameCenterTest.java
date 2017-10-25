8e9b56a406d4eef425cc4f25707032fe93001e26
tdvenky <31875310+tdvenky@users.noreply.github.com>
2017-10-25 13:15:57 -0400

    RemovePlayerGame test

--- src/test/java/com.webcheckers/appl/GameCenterTest.java
+++ src/test/java/com.webcheckers/appl/GameCenterTest.java
@@ -6,7 +6,14 @@ import static org.junit.Assert.*;
 
 public class GameCenterTest {
     @Test
-    public void getGames() throws Exception {
+    public void removePlayerGames() throws Exception {
+        GameCenter cut=new GameCenter();
+        String playerName="james bond";
+        //cut.registerPlayer(playerName);
+        assertTrue("playerName added",cut.registerPlayer(playerName) );
+        //cut.removePlayer(playerName);
+        assertTrue("playerName removed",cut.removePlayer(playerName) );
     }
+
 
 }
